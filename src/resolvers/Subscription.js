c557d03243b8750e2cecd3f56ffbb83d377ba0f2
mbouzi <mrbouzi@gmail.com>
2018-03-26 20:24:16 -0400

    create vote comment subscriptions

---
+++ src/resolvers/Subscription.js
@@ -0,0 +1,22 @@
+const newComment = {
+  subscribe: (parent, args, ctx, info) => {
+    return ctx.db.subscription.comment(
+      { },
+      info,
+    )
+  },
+}
+
+const newVote = {
+  subscribe: (parent, args, ctx, info) => {
+    return ctx.db.subscription.vote(
+      { },
+      info,
+    )
+  },
+}
+
+module.exports = {
+  newComment,
+  newVote,
+}
